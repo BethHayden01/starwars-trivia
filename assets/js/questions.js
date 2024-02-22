d109def0992b7aa62dcdc630c7fe01d4376fa4c3
BethHayden01 <bethanyhayden12345@outlook.com>
2024-02-22 02:42:17 +0000

    Next issue is in line 38, The app is not registering which answer is correct

---
+++ assets/js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        number: 1,
+        question: "Who is Luke Skywalker's father?",
+        answer: "Darth vader",
+        options: [
+            "Obi-Wan Kenobi",
+            "Darth Vader",
+            "Emperor Palpatine",
+            "Count Dooku"
+        ]
+    },
+    {
+        number: 2,
+        question: "What is the name of Han Solo's ship?",
+        answer:"Millennium Falcon",
+        options: [
+            "Millennium Falcon",
+            "Star Destroyer",
+            "X-wing",
+            "Slave I"
+        ]
+    },
+    {
+        number: 3,
+        question: "Who trained Anakin Skywalker as a Jedi?",
+        answer: "Qui-Gon Jinn",
+        options: [
+            "Yoda",
+            "Qui-Gon Jinn",
+            "Mace Windu",
+            "Obi-Wan Kenobi"
+        ]
+    },
+    {
+        number: 4,
+        question: "What is the name of the planet where Rey finds Luke Skywalker in The Force Awakens?",
+        answer: "Ahch-To",
+        options: [
+            "Jakku",
+            "Ahch-To", 
+            "Takodana",
+            "Ahch-To"
+        ]
+    },
+    {
+        number: 5,
+        question: "What is the name for a Robot in Star Wars?",
+        answer: "Droid",
+        options: [
+            "Bot",
+            "Robot",
+            "Droid",
+            "Machine"
+        ]
+    },
+    {
+        number: 6,
+        question: "Where did the Clone Wars begin?",
+        answer: "Geonosis",
+        options: [
+            "Tatooine",
+            "Geonosis",
+            "Naboo",
+            "Coruscant"
+        ]
+    },
+    {
+        number: 7,
+        question: "What is Baby Yoda's real name?",
+        answer: "Grogu",
+        options: [
+            "Grogu",
+            "Alfred",
+            "Yoda 2",
+            "Dillan"
+        ]
+    },
+    {
+        number: 8,
+        question: "Which planet did Obi-wan take Luke to after he was born",
+        answer: "Tatooine",
+        options: [
+            "Kamino",
+            "Tatooine", 
+            "Bespin", 
+            "Alderaan"
+        ]
+    },
+    {
+        number: 9,
+        question: "Who was Anakin Skywalker's Padawan?",
+        answer: "Ahsoka Tano",
+        options: [
+            "Yoda",
+            "Luke Skywalker",
+            "Rey",
+            "Ahsoka Tano"
+        ]
+    },
+    {
+        number: 10,
+        question: "Who is Emperor Palpatine's granddaughter?",
+        answer: "Rey",
+        options: [
+            "Luke Skywalker",
+            "Ben solo", 
+            "Rey",
+            "Kylo Ren"
+        ]
+    },
+];
